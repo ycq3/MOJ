1befe02aa442c27c85db2a9b68fa495f18f8a555
皮皮强 <23733814+ycq3@users.noreply.github.com>
2020-02-02 11:46:49 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MNNU_ACM_OnlineJudge') }}</title>
